920ff52c34cd8a7248cbde3fb9c602ee0e75ff8b
Yakumo-zi <1477717283@qq.com>
2024-09-04 14:23:38 +0000

    feat(neovim):format lazy.lua code

--- nvim/lua/core/lazy.lua
+++ nvim/lua/core/lazy.lua
@@ -1,34 +1,34 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
 local ok, lazy = pcall(require, "lazy")
 if not ok then
-	return
+  return
 end
 
 lazy.setup({
-	spec = {
-		{ import = "plugins" },
-		{ import = "plugins.lsp" },
-		{ import = "plugins.lsp.enhance" },
-		{ import = "plugins.ui" },
-		{ import = "plugins.utils" },
-	},
-	ui = {
-		border = "rounded",
-		size = {
-			width = 0.8,
-			height = 0.8,
-		},
-	},
+  spec = {
+    { import = "plugins" },
+    { import = "plugins.lsp" },
+    { import = "plugins.lsp.enhance" },
+    { import = "plugins.ui" },
+    { import = "plugins.utils" },
+  },
+  ui = {
+    border = "rounded",
+    size = {
+      width = 0.8,
+      height = 0.8,
+    },
+  },
 })
