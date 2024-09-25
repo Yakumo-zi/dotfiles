a14e0405f7b32728cafa40f86044010182556a01
Yakumo-zi <1477717283@qq.com>
2024-09-25 23:41:29 +0800

    feat:remove flake and home-manager

---
+++ setup.sh
@@ -0,0 +1,44 @@
+sudo rm -rf /nix
+mv ~/.profile ~/.profile.backup-"${USER}"
+touch ~/.profile
+sh <(curl -L https://nixos.org/nix/install)
+
+if [ -e /home/yakumo/.nix-profile/etc/profile.d/nix.sh ]; then eval '. /home/yakumo/.nix-profile/etc/profile.d/nix.sh'; fi # added by Nix installer
+sudo chown -R "${USER}" /nix
+nix-channel --update
+
+Pkgs=(
+  'fastfetch'
+  'neovim'
+	'git'
+	'curl'
+	'unzip'
+	'fzf'
+	'ripgrep'
+	'lazygit'
+	'gcc14'
+	'go'
+	'nodejs_20'
+	'pnpm'
+	'rustup'
+	'cmake'
+	'gnumake'
+	'docker_27'
+	'clang-tools'
+	'lua-language-server'
+	'stylua'
+	'gopls'
+	'nixd'
+	'nodePackages_latest.bash-language-server'
+)
+
+install_pkgs=""
+for i in "${!Pkgs[@]}"; do
+	Pkgs[i]="nixpkgs.${Pkgs[$i]}"
+	install_pkgs=${install_pkgs}" "${Pkgs[$i]}
+done
+# echo "nix-env -iA ${install_pkgs}"
+
+eval "nix-env -iA ${install_pkgs}"
+
+ln -s "$(PWD)/nvim" "$(HOME)/.config/nvim"
