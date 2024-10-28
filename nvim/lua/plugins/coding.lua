06f88d87c3463b04299f6f81fdbf8e55013d9865
Yakumo-zi <1477717283@qq.com>
2024-10-28 10:12:43 +0800

    feat:add python and ts support

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -52,5 +52,26 @@ return {
         },
       },
     },
+  },
+  {
+    "pmizio/typescript-tools.nvim",
+    ft = { "typescript", "typescriptreact", "javascriptreact", "javascript" },
+    dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+    opts = {},
+    config = function()
+      require("typescript-tools").setup {
+        on_attach =
+            function(client, bufnr)
+              client.server_capabilities.documentFormattingProvider = false
+              client.server_capabilities.documentRangeFormattingProvider = false
+            end,
+        settings = {
+          jsx_close_tag = {
+            enable = true,
+            filetypes = { "javascriptreact", "typescriptreact" },
+          }
+        }
+      }
+    end
   }
 }
