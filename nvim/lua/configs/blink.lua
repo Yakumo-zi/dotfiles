05dfa54b24f6cc09208e794e0984a2959bf00d21
Yakumo-zi <yakumozi1110@gmail.com>
2025-05-13 17:24:59 +0800

    feat(nvim):remove ai plugin

--- nvim/lua/configs/blink.lua
+++ nvim/lua/configs/blink.lua
@@ -15,15 +15,6 @@ local opts = {
       },
     },
   },
-  sources = {
-    default = { "avante", "lsp", "path", "snippets", "buffer" },
-    providers = {
-      avante = {
-        module = "blink-cmp-avante",
-        name = "Avante",
-      },
-    },
-  },
 }
 
 return opts
