60dadc1b8a5e14e0a498666af24f2b64e3961b47
Yakumo-zi <yakumozi1110@gmail.com>
2026-01-12 10:19:10 +0800

    feat: oil float

--- nvim/lua/plugins/utils.lua
+++ nvim/lua/plugins/utils.lua
@@ -4,11 +4,22 @@ return {
     "stevearc/oil.nvim",
     ---@module 'oil'
     ---@type oil.SetupOpts
-    opts = {},
+    opts = {
+      float = {
+        padding = 5, -- 增加内边距让它不要贴着屏幕边缘
+        max_width = 120, -- 限制最大宽度，防止在大屏上太宽
+        max_height = 30, -- 限制高度
+        border = "rounded",
+        win_options = {
+          winblend = 0, -- 透明度 (0-100)，需要终端支持
+        },
+        override = function(conf) return conf end,
+      },
+    },
     dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
     lazy = false,
     keys = {
-      { "-", "<CMD>Oil<CR>", desc = "Open parent directory" },
+      { "-", "<CMD>Oil --float<CR>", desc = "Open parent directory" },
     },
   },
 }
